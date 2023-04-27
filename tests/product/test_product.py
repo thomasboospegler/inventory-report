707a131512813978347502dfbed6bd8d5c1be10e
Thomas Boos Pegler <tboospegler@gmail.com>
2023-04-27 17:12:45 -0400

    test: add test for class Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1, "produto", "empresa", "01/01/2021",
+        "01/01/2022", "123", "armazenamento"
+    )
+    assert produto.id == 1
+    assert produto.nome_do_produto == "produto"
+    assert produto.nome_da_empresa == "empresa"
+    assert produto.data_de_fabricacao == "01/01/2021"
+    assert produto.data_de_validade == "01/01/2022"
+    assert produto.numero_de_serie == "123"
+    assert produto.instrucoes_de_armazenamento == "armazenamento"
+
+    assert (
+        produto.__repr__() ==
+        "O produto produto fabricado em 01/01/2021 por empresa com validade"
+        " até 01/01/2022 precisa ser armazenado armazenamento."
+    )
