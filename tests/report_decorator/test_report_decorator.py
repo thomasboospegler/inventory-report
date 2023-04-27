125cd7b015fc8922ce3d80cdab96fa0caa943239
Thomas Boos Pegler <tboospegler@gmail.com>
2023-04-27 18:42:04 -0400

    test: add test for colored report

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,29 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+
+colored_strings = [
+    "\033[32mData de fabricação mais antiga:\033[0m",
+    "\033[32mData de validade mais próxima:\033[0m",
+    "\033[32mEmpresa com mais produtos:\033[0m",
+    "\033[36m2021-03-10\033[0m",
+    "\033[36m2023-05-09\033[0m",
+    "\033[31mAmbev\033[0m",
+]
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    report = ColoredReport(SimpleReport).generate(
+        [
+            {
+                "id": 1,
+                "nome_do_produto": "Skol",
+                "nome_da_empresa": "Ambev",
+                "data_de_fabricacao": "2021-03-10",
+                "data_de_validade": "2023-05-09",
+                "numero_de_serie": "FR48",
+                "instrucoes_de_armazenamento": "Gelado",
+            }
+        ]
+    )
+    for string in colored_strings:
+        assert string in report
