8f46c25521cd93d80e50e88ad1c22e424a1332d2
Thomas Boos Pegler <tboospegler@gmail.com>
2023-04-27 18:27:03 -0400

    test: add test for Product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        "1",
+        "Cerveja",
+        "Ambev",
+        "01/01/2020",
+        "01/01/2021",
+        "123456",
+        "gelado",
+    )
+    assert (
+        produto.__repr__() == (
+            "O produto Cerveja fabricado em 01/01/2020 por Ambev com "
+            "validade até 01/01/2021 precisa ser armazenado gelado."
+        )
+    )
